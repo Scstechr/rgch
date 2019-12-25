9141f2ce40b8e2d3f946703a9cbf62c267d70cfb
Scstechr <teufelkonig@gmail.com>
2019-12-26 03:32:08 +0900

    check with -- and -

--- src/arg.rs
+++ src/arg.rs
@@ -1,7 +1,5 @@
-
 use std::env;
-
-use crate::Config;
+use crate::{Config, Arg};
 
 pub fn parse_defaults() -> Config {
     let config = Config {
@@ -13,5 +11,13 @@ pub fn parse_defaults() -> Config {
 
 pub fn parse_arguments() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    for a in args.iter() {
+        if a.starts_with("--") {
+            println!("case a {}", a);
+        } else if a.starts_with("-") {
+            println!("case b {}", a);
+        }
+    }
     args
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,11 @@ pub struct Config {
     commit: bool,
     push: bool
 }
+
+pub struct Arg {
+    short: String,
+    long: String,
+    types: String,
+    exp: String,
+}
+
