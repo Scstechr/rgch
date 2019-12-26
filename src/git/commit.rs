f5b78a9f4a5f010774af948740e74198ded18ca1
Scstechr <teufelkonig@gmail.com>
2019-12-27 02:42:24 +0900

    check status before commit

--- src/git/commit.rs
+++ src/git/commit.rs
@@ -1,22 +1,25 @@
 use crate::colors::{G, X};
+use crate::git::status::status;
 use crate::proc::execute;
 use std::io::{stdin, stdout, Write};
 
 pub fn commit() {
-    print!("\n{} Enter commit message: {}", G, X);
-    let mut s = String::new();
-    let _ = stdout().flush();
-    stdin().read_line(&mut s).expect("-a");
-    if let Some('\n') = s.chars().next_back() {
-        s.pop();
+    if status() {
+        print!("\n{} Enter commit message: {}", G, X);
+        let mut s = String::new();
+        let _ = stdout().flush();
+        stdin().read_line(&mut s).expect("-a");
+        if let Some('\n') = s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r') = s.chars().next_back() {
+            s.pop();
+        }
+        let command = if s != "" {
+            format!("git commit -a -m \"{}\"", s)
+        } else {
+            "git commit -a".to_string()
+        };
+        execute(&command);
     }
-    if let Some('\r') = s.chars().next_back() {
-        s.pop();
-    }
-    let command = if s != "" {
-        format!("git commit -a -m \"{}\"", s)
-    } else {
-        "git commit -a".to_string()
-    };
-    execute(&command);
 }
