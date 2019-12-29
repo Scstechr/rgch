1fa0edd417937cc51205e414c72d88940a49af1c
Scstechr <teufelkonig@gmail.com>
2019-12-29 20:45:29 +0900

    set_clone_{url, branch, dir}

--- src/git/clone.rs
+++ src/git/clone.rs
@@ -17,13 +17,9 @@ fn set_clone_url(given: &str) -> String {
     url.replace("@", "https://github.com/")
 }
 
-pub fn clone(given_url: &str, given_branch: &str, given_input: bool) {
-    // println!("{}", given_input);
-    println!("{}>> Cloning remote repository...{}", G, X);
-    // println!("{}, {}", given_url, given_branch);
-    let url = set_clone_url(&given_url);
-    let branch = if given_input {
-        given_branch.to_string()
+fn set_clone_branch(given: &str, flag: bool) -> String {
+    let branch = if flag {
+        given.to_string()
     } else if confirm("Is it master branch") {
         "master".to_string()
     } else {
@@ -35,12 +31,26 @@ pub fn clone(given_url: &str, given_branch: &str, given_input: bool) {
         }
     };
     println!("{}   {} Remote branch set to: {}{}", F, RET, X, branch);
+    branch
+}
+
+fn set_clone_dir(url: &str) -> String {
     let name: Vec<&str> = url.split('/').collect();
     let name = name.last().unwrap();
     println!(
         "{}   {} Cloning remote repository to: {}{}",
         F, RET, X, name
     );
+    name.to_string()
+}
+
+pub fn clone(given_url: &str, given_branch: &str, given_input: bool) {
+    // println!("{}", given_input);
+    println!("{}>> Cloning remote repository...{}", G, X);
+    // println!("{}, {}", given_url, given_branch);
+    let url = set_clone_url(&given_url);
+    let branch = set_clone_branch(&given_branch, given_input);
+    let name = set_clone_dir(&url);
     let command = format!("git clone -b {} {} {}", branch, url, name);
     execute(&command);
     exit(0);
