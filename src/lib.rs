b6c23b4e25d1668adfdda6f554892c6ec7956872
Scstechr <teufelkonig@gmail.com>
2020-01-03 17:05:41 +0900

    make fields of Opt & Arg pub. transfer lib.rs/run() -> main.rs/main()

--- src/lib.rs
+++ src/lib.rs
@@ -7,95 +7,21 @@ pub mod misc;
 pub mod proc;
 pub mod version;
 
-use crate::{
-    arg::parse_arguments,
-    git::{
-        add::{add, silence_add},
-        branch::set_branch,
-        clone::clone,
-        commit::commit,
-        diff::diff,
-        log::log,
-        pull::pull,
-        push::push,
-        reset::reset,
-        status::{check_status, is_status_clean, short_status},
-    },
-    help::help,
-    version::{short_version, version},
-};
-
 #[derive(Debug)]
 pub struct Opt {
-    save: bool,
-    flag: bool,
-    value: String,
+    pub save: bool,
+    pub flag: bool,
+    pub value: String,
 }
 
 #[derive(Debug)]
 pub struct Arg {
-    short: &'static str,
-    long: &'static str,
-    types: &'static str,
-    save: bool,
-    flag: bool,
-    category: &'static str,
-    value: String,
-    exp: &'static str,
-}
-
-pub fn run() {
-    short_version();
-    let args = parse_arguments();
-
-    //     for arg in &args {
-    //         println!("{:?}", arg);
-    //     }
-
-    if args["help"].flag {
-        help();
-    } else if args["version"].flag {
-        version();
-    } else if args["clone"].flag {
-        clone(
-            &args["clone"].value,
-            &args["branch"].value,
-            args["branch"].flag,
-        );
-    } else {
-        let branch = set_branch(&args["branch"].value, &args["gitdir"].value);
-
-        if args["log"].flag {
-            log();
-        }
-
-        if args["pull"].flag {
-            pull(&args["remote"].value, &args["branch"].value);
-        }
-
-        if is_status_clean() {
-            diff(args["verbose"].flag);
-        }
-
-        if args["commit"].flag {
-            commit(
-                &args["add"].value,
-                &args["commit"].value,
-                args["force"].flag,
-            );
-        } else {
-            if args["add"].flag {
-                add(&args["add"].value, args["force"].flag);
-            } else {
-                silence_add(&args["add"].value, args["force"].flag);
-            }
-            if check_status() {
-                short_status();
-            }
-            reset();
-        }
-        if args["push"].flag {
-            push(&branch);
-        }
-    }
+    pub short: &'static str,
+    pub long: &'static str,
+    pub types: &'static str,
+    pub save: bool,
+    pub flag: bool,
+    pub category: &'static str,
+    pub value: String,
+    pub exp: &'static str,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,75 @@
 extern crate rgch;
 
+use rgch::{
+    arg::parse_arguments,
+    git::{
+        add::{add, silence_add},
+        branch::set_branch,
+        clone::clone,
+        commit::commit,
+        diff::diff,
+        log::log,
+        pull::pull,
+        push::push,
+        reset::reset,
+        status::{check_status, is_status_clean, short_status},
+    },
+    help::help,
+    version::{short_version, version},
+};
+
 fn main() {
-    rgch::run();
+    short_version();
+    let args = parse_arguments();
+
+    //     for arg in &args {
+    //         println!("{:?}", arg);
+    //     }
+
+    if args["help"].flag {
+        help();
+    } else if args["version"].flag {
+        version();
+    } else if args["clone"].flag {
+        clone(
+            &args["clone"].value,
+            &args["branch"].value,
+            args["branch"].flag,
+        );
+    } else {
+        let branch = set_branch(&args["branch"].value, &args["gitdir"].value);
+
+        if args["log"].flag {
+            log();
+        }
+
+        if args["pull"].flag {
+            pull(&args["remote"].value, &args["branch"].value);
+        }
+
+        if is_status_clean() {
+            diff(args["verbose"].flag);
+        }
+
+        if args["commit"].flag {
+            commit(
+                &args["add"].value,
+                &args["commit"].value,
+                args["force"].flag,
+            );
+        } else {
+            if args["add"].flag {
+                add(&args["add"].value, args["force"].flag);
+            } else {
+                silence_add(&args["add"].value, args["force"].flag);
+            }
+            if check_status() {
+                short_status();
+            }
+            reset();
+        }
+        if args["push"].flag {
+            push(&branch);
+        }
+    }
 }
