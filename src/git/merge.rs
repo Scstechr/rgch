e181fc7b207bf47a74a777c4c9626bdd13fdc2ac
Scstechr <teufelkonig@gmail.com>
2020-03-16 23:26:35 +0900

    copy args in merge()

--- src/git/merge.rs
+++ src/git/merge.rs
@@ -1,14 +1,35 @@
 use crate::git::{branch, checkout, pull};
 use crate::misc::beep;
 use crate::proc;
+use crate::Opt;
+use std::collections::HashMap;
 
-pub fn merge(given_remote: &str, given_branch: &str) {
+pub fn merge<S: ::std::hash::BuildHasher + Default>(args: &HashMap<String, Opt, S>) {
     beep();
     println!("EXPERIMENTAL FEATURE!");
     let branch = branch::get_branch();
-    checkout::checkout("master");
-    pull::pull(given_remote, given_branch, false);
-    let command = format!("git merge {} --no-ff", branch);
-    proc::execute(&command);
-    branch::delete_branch(&branch);
+    let mut args_copy: HashMap<String, Opt> = HashMap::new();
+    for (a, o) in args.iter() {
+        args_copy.insert(
+            a.to_string(),
+            match a as &str {
+                "branch" => Opt {
+                    save: o.save,
+                    flag: o.flag,
+                    value: "master".to_string(),
+                },
+                _ => Opt {
+                    save: o.save,
+                    flag: o.flag,
+                    value: o.value.clone(),
+                },
+            },
+        );
+    }
+    // args["branch"].value = "master".to_string();
+    // checkout::checkout(&args["branch"].value);
+    // pull::pull(&args["remote"].value, &args["branch"].value, false);
+    // let command = format!("git merge {} --no-ff", branch);
+    // proc::execute(&command);
+    // branch::delete_branch(&branch);
 }
