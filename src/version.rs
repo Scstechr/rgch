3f8e750cd6ad4fcb1c2dc3f0c1b19ba8bc26680f
Scstechr <teufelkonig@gmail.com>
2020-03-17 00:19:21 +0900

    version update

--- src/version.rs
+++ src/version.rs
@@ -3,8 +3,8 @@ use std::process::exit;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const BLANK: &str = "            ";
-const GITREV: &str = "dbeafbbc";
-const DATE: &str = "2020-03-16";
+const GITREV: &str = "a6d6ea25";
+const DATE: &str = "2020-03-17";
 const RUSTC: &str = "rustc 1.42.0 (b8cedc004 2020-03-09)\n";
 const FMT: &str = "clippy 0.0.212 (4ee12063 2020-02-01)\n";
 const CLIPPY: &str = "rustfmt 1.4.11-stable (9eb4b564 2020-01-29)\n";
