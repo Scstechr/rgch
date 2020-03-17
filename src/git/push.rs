989d33c40699a8f8de0a238ba8e194096795410d
Scstechr <teufelkonig@gmail.com>
2020-03-18 03:46:30 +0900

    redefined push() w/o message

--- src/git/push.rs
+++ src/git/push.rs
@@ -3,8 +3,8 @@ use crate::git::pull;
 use crate::proc::execute;
 
 pub fn push(remote: &str, branch: &str) {
-    let string = format!("{a}Pull before push{x}", a = ARS, x = X);
-    println!("{}", string);
+    // let string = format!("{a}Pull before push{x}", a = ARS, x = X);
+    // println!("{}", string);
     pull::pull(remote, branch, false);
     let string = format!("git push {} {}", remote, branch);
     execute(&string);
